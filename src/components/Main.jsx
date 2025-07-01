e468a2f7fcd8c7ab4f68164e10fa4316f76d02ca
Janan Al-Hassani <janan.alhassani@gmail.com>
2025-07-01 19:38:28 -0400

    Maintain an ingredient and an array of ingredients as state and display as unordered list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,34 @@
+import { useState } from 'react'
 import './Main.css'
 
 function Main() {
+    const [ingredient, setIngredient] = useState("")
+    const [ingredients, setIngredients] = useState([])
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        if (!ingredient.trim()) return
+        setIngredients(prev => [...prev, ingredient])
+        setIngredient("")
+    }
+
     return (
         <main>
-            <form className="add-ingredient">
+            <form className="add-ingredient" onSubmit={handleSubmit}>
                 <input
                     type="text"
                     className='add-ingredient-input'
                     placeholder="e.g. Parmesan" 
                     aria-label="Add ingredient"
+                    name='ingredient'
+                    value={ingredient}
+                    onChange={e => setIngredient(e.target.value)}
                 />
                 <button className='add-ingredient-button'>+ Add Ingredient</button>
             </form>
+            <ul>
+                {ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)}
+            </ul>
         </main>
     )
 }
