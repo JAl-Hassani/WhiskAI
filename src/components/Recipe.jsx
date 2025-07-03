056d14698ef66114eaa9f818dae62c39e4cd8695
Janan Al-Hassani <janan.alhassani@gmail.com>
2025-07-02 20:54:02 -0400

    Use react-markdown to format the generated recipe

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -1,6 +1,14 @@
+import ReactMarkdown from 'react-markdown'
+
+function MarkdownViewer({ markdownText }) {
+    return <ReactMarkdown>{markdownText}</ReactMarkdown>
+}
+
 function Recipe({ recipe }) {
     return (
-        <section>{recipe}</section>
+        <section>
+            <MarkdownViewer markdownText={recipe} />
+        </section>
     )
 }
 
